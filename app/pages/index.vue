f6ce76b34910e1c4f307456ba60d3aef23bccdd8
maulanairfanf <maulanairfanf@gmail.com>
2025-10-06 11:37:41 +0700

    add button to articles

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fuga unde
-    commodi placeat consequatur quae modi repudiandae! Officia voluptates nihil
-    ipsum corrupti quos consequuntur rerum possimus quas, modi voluptatibus
-    nemo!
+  <div class="min-h-screen flex justify-center items-center bg-gray-50">
+    <NuxtLink
+      to="/articles"
+      class="inline-flex items-center px-4 py-2 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+    >
+      Go To Articles
+      <svg
+        class="w-4 h-4 ml-2"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
+    </NuxtLink>
   </div>
 </template>
 
